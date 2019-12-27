b812510cbf6cf0d70b247788b5731fd88a254ef8
Osmely Fernandez <osmelyf@gmail.com>
2019-12-27 12:51:40 -0500

    Commit 0

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Osmely Fernandez on 12/27/19.
+//  Copyright © 2019 Osmely Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
